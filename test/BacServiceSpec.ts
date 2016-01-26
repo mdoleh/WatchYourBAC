eacfe68767b2ce1500418bcd99739dbc2c629620
Ryan Wittibschlager <ryanwittibschlager@Ryans-MacBook-Pro-3.local>
2016-01-25 20:34:25 -0500

    fixed nodemon issue, fixed unit tests

--- test/BacServiceSpec.ts
+++ test/BacServiceSpec.ts
@@ -1,39 +1,79 @@
 import {BacService} from '../src/BacService';
+import {Person} from '../src/Person';
 
 describe("BacService", () => {
   let bacService : BacService;
   
   beforeEach(() => {
     bacService = new BacService();
+    // oz drank, body weight, genderIsMale, hours drinking
   });
   
   it("BAC should be zero with no alcohol consumed", () => {
-    expect(bacService.calcBAC(0, 180, true, 2) <= 0).toBe(true);
+      var person : Person = {
+          bodyWeight: 180,
+          gender: "Male",
+          hoursDrinking: 2
+      }
+      bacService.init(person);
+    expect(bacService.calcBAC(0) <= 0).toBe(true);
   });
   
   it("BAC should be higher with a lighter weight person", () => {
-    let bacLightWeight = bacService.calcBAC(3, 180, true, 2);
-    let bacHeavyWeight = bacService.calcBAC(3, 150, true, 2);
+      var person : Person = {
+          bodyWeight: 180,
+          gender: "Male",
+          hoursDrinking: 2
+      }
+      bacService.init(person);
+    let bacLightWeight = bacService.calcBAC(3);
+    person.bodyWeight = 150;
+    let bacHeavyWeight = bacService.calcBAC(3);
     expect(bacLightWeight < bacHeavyWeight).toBe(true);
   });
   
   it("BAC should be lower with more time passed since drinking", () => {
-    let bacLessTime = bacService.calcBAC(3, 180, true, 1);
-    let bacMoreTime = bacService.calcBAC(3, 180, true, 3);
+      var person : Person = {
+          bodyWeight: 180,
+          gender: "Male",
+          hoursDrinking: 1
+      }
+      bacService.init(person);
+    let bacLessTime = bacService.calcBAC(3);
+    person.hoursDrinking = 3;
+    let bacMoreTime = bacService.calcBAC(3);
     expect(bacMoreTime < bacLessTime).toBe(true);
   });
   
   it("BAC should be higher with more alcohol consumed", () => {
-    let bacLessAlcohol = bacService.calcBAC(3, 180, true, 2);
-    let bacMoreAlcohol = bacService.calcBAC(5, 180, true, 2);
+      var person : Person = {
+          bodyWeight: 180,
+          gender: "Male",
+          hoursDrinking: 2
+      }
+      bacService.init(person);
+    let bacLessAlcohol = bacService.calcBAC(3);
+    let bacMoreAlcohol = bacService.calcBAC(5);
     expect(bacLessAlcohol < bacMoreAlcohol).toBe(true);
   });
   
   it("should compute BAC on a man", () => {
-    expect(bacService.calcBAC(24, 180, true, 2)).toEqual(0.47029333333333323);
+      var person : Person = {
+          bodyWeight: 180,
+          gender: "Male",
+          hoursDrinking: 2
+      }
+      bacService.init(person);
+    expect(bacService.calcBAC(24)).toEqual(0.47029333333333323);
   });
   
   it("should compute BAC on a woman", () => {
-    expect(bacService.calcBAC(24, 180, false, 2)).toEqual(0.4223199999999999);
+      var person : Person = {
+          bodyWeight: 180,
+          gender: "Female",
+          hoursDrinking: 2
+      }
+      bacService.init(person);
+    expect(bacService.calcBAC(24)).toEqual(0.4223199999999999);
   });
 });
