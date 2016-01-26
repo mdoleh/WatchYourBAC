404197b08ac98cfb215633825175c0437e026b92
Ryan Wittibschlager <ryanwittibschlager@Ryans-MacBook-Pro-3.local>
2016-01-26 13:49:11 -0500

    added more details to beer details

--- src/BeerDetailsComponent.ts
+++ src/BeerDetailsComponent.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from 'angular2/core';
 import {RouteParams} from 'angular2/router';
+import {API_RESPONSE_TEMPLATE} from './Constants';
+import {BeerService} from './BeerService';
 
 @Component({
   selector: "app",
@@ -7,15 +9,26 @@ import {RouteParams} from 'angular2/router';
   template: `
     <h1>Beer  Details</h1>
     <div>beer ID: {{beerId}}</div>
+    <div>Name: {{beer.nameDisplay}}</div>
+    <div>Description: {{beer.description}}</div>
+    <div>ABV: {{beer.abv}}</div>
   `,
+  providers: [BeerService]
 })
 export class BeerDetailsComponent implements OnInit {
   private beerId : string;
     
-  constructor(routeParams: RouteParams){
+  constructor(
+      private _beerService : BeerService,
+      routeParams: RouteParams){
       this.beerId = routeParams.get("id")
   }
+  beer = [API_RESPONSE_TEMPLATE];
   ngOnInit() {
       // service call to get beer by id and use results in template
+    this._beerService.searchById(this.beerId)
+      .subscribe(res => {
+        this.beer = res.json().data;
+    });
   }
 }
