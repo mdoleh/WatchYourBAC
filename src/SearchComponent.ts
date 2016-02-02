707b5c524a8109d48754bcc6a0023f04d8599b40
Ryan Wittibschlager <ryanwittibschlager@Ryans-MacBook-Pro-3.local>
2016-02-02 12:38:42 -0500

    SC 1 - search and clear buttons added

--- src/SearchComponent.ts
+++ src/SearchComponent.ts
@@ -9,39 +9,49 @@ import {SearchButtonsComponent} from './SearchButtonsComponent';
   styles: ['h1 {color: red}'],
   template: `
     <h1>Beer Search</h1> <br />
-    Search: <input #beerItem (keyup)="onKey(beerItem.value)" />
-    <search-buttons (previousPage)="previousPage(beerItem.value, rawResponse.currentPage)" (nextPage)="nextPage(beerItem.value, rawResponse.currentPage)" [shouldDisableNext]="!canPageForward()" [shouldDisablePrevious]="!canPageBackward()"></search-buttons>
+    Search: <input [(ngModel)]="beerName"/>
+    <input (click)="search(beerName)" type="button" value="Search" />
+    <input (click)="reset()" type="button" value="Clear" />
+    <search-buttons (previousPage)="previousPage(beerName, rawResponse.currentPage)" (nextPage)="nextPage(beerName, rawResponse.currentPage)" [shouldDisableNext]="!canPageForward()" [shouldDisablePrevious]="!canPageBackward()"></search-buttons>
     <div *ngIf="rawResponse.numberOfPages">Viewing page {{rawResponse.currentPage}} of {{rawResponse.numberOfPages}}</div>
     <div *ngFor="#beer of beers" class="api-data">
       <ul>
         <li (click)="beerSelected(beer.id)">{{beer.nameDisplay}}</li>
       </ul>
     </div>
-    <search-buttons *ngIf="rawResponse.numberOfPages" (previousPage)="previousPage(beerItem.value, rawResponse.currentPage)" (nextPage)="nextPage(beerItem.value, rawResponse.currentPage)" [shouldDisableNext]="!canPageForward()" [shouldDisablePrevious]="!canPageBackward()"></search-buttons>
+    <search-buttons *ngIf="rawResponse.numberOfPages" (previousPage)="previousPage(beerName, rawResponse.currentPage)" (nextPage)="nextPage(beerName, rawResponse.currentPage)" [shouldDisableNext]="!canPageForward()" [shouldDisablePrevious]="!canPageBackward()"></search-buttons>
   `,
   providers: [BeerService],
   directives: [SearchButtonsComponent]
 })
 export class SearchComponent implements OnInit {
+    private beerName : string;
+    beers : Beer[];
+    rawResponse : RawResponse = new RawResponse();
+    callComplete : boolean = false;
+    
     constructor(
         private _beerService : BeerService,
         private _router: Router
     ){}
-    beerName : string;
-    beers : Beer[];
-    rawResponse : RawResponse = new RawResponse();
-    callComplete : boolean = false;
   
     ngOnInit() {
-        this.rawResponse.currentPage = 0;
+        this.reset();
     }
 
-    onKey(value : string) {
+    search(value : string) {
         if (value) {
             this.searchBeer(value);
         } else {
-            this.beers = [];
+            this.reset();
         }
+    }
+    
+    reset() {
+        this.beers = [];
+        this.beerName = "";
+        this.rawResponse.numberOfPages = 0;
+        this.rawResponse.currentPage = 0;
     }
     
     nextPage(beerName : string, currentPage : number) {
